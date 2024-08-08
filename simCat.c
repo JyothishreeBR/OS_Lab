3c4ca24fe4c9e3c46b7eb224b7917a6c9d871dd5
Jyothishree <jyothishree2023@gmail.com>
2024-08-09 00:11:58 +0530

    simulation of cat view only

---
+++ simCat.c
@@ -0,0 +1,24 @@
+
+//simulation of Cat command for view only purposeq
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+
+ void main(int argc,char*argv[])
+{
+ FILE *fp;char ch;
+ if(argc<2)
+ {
+ printf("File not entered ");
+ exit(1);
+ }
+ fp=fopen(argv[1],"r");
+ if(fp==NULL){
+ printf("File doesn't exist\n");
+ exit(1);
+ }
+ while((ch=fgetc(fp))!=EOF)
+ printf("%c",ch);
+ fclose(fp);
+ printf("\n");
+}
